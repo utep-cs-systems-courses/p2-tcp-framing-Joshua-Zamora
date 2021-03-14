4a9c757ad9367fdbc0ff291b1ca16f2e1456b24f
ZanteZyaire <jazamora6@miners.utep.edu>
2021-03-14 13:47:22 -0600

    Added capability to recieve files from server

--- file-transfer/fileClient.py
+++ file-transfer/fileClient.py
@@ -5,7 +5,8 @@ import socket, sys, re, time, os
 sys.path.append("../lib")       # for params
 import params
 
-def send_file(local_file, remote_file, host):
+
+def create_socket(host):
     switchesVarDefaults = (
         (('-s', '--server'), 'server', "127.0.0.1:50001"),
         (('-d', '--delay'), 'delay', "0"),
@@ -27,7 +28,7 @@ def send_file(local_file, remote_file, host):
         print("Can't parse server:port from '%s'" % server)
         sys.exit(1)
 
-        s = None
+    s = None
     for res in socket.getaddrinfo(host, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
         af, socktype, proto, canonname, sa = res
         try:
@@ -56,11 +57,16 @@ def send_file(local_file, remote_file, host):
         print(f"sleeping for {delay}s")
         time.sleep(delay)
         print("done sleeping")
-    
+
+    return s
+
+
+def send_file(local_file, remote_file, host):
+    s = create_socket(host)
     file_descriptor = os.open(local_file, os.O_RDONLY)
     file_size = os.path.getsize(local_file)
     chunk = os.read(file_descriptor, 1024)
-    s.send("{0}:{1}".format(len(remote_file), remote_file).encode())
+    s.send("{0}:s{1}".format(len(remote_file) + 1, remote_file).encode())
     s.send("{0}:{1}".format(file_size, chunk.decode()).encode())
     
     while True:
@@ -69,5 +75,27 @@ def send_file(local_file, remote_file, host):
             break
         else:
             s.send(chunk)
+    
+    s.close()
+
+
+def recieve_file(local_file, remote_file, host):
+    s = create_socket(host)
+    s.send("{0}:r{1}".format(len(remote_file) + 1, remote_file).encode())
+    data = s.recv(1024).decode()
+    delim = data.index(":")
+    msg_length = int(data[:delim])
+    data = data[delim + 1:]
+    file_descriptor = os.open(local_file, os.O_CREAT | os.O_WRONLY)
+    
+    full_msg = ""
+    while data:
+        full_msg += data
+        msg_length -= len(data)
+        data = s.recv(1024).decode()
+            
+        if msg_length == 0:
+            os.write(file_descriptor, full_msg.encode())
+            break
     
     s.close()

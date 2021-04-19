f71a1d76a6bcb891d27ca12bc590bb3a22cbb96a
ZanteZyaire <jazamora6@miners.utep.edu>
2021-04-18 21:52:23 -0600

    Fixed infinite loop

--- file-transfer/myreadline.py
+++ file-transfer/myreadline.py
@@ -1,30 +1,23 @@
 from os import read, write
 
-buff = ""
-data = ""
 
 def myreadline():
-    global buff
-    global data
-    
     index = 0
     line = ""
-    
-    if buff == "":
-        buff = read(0, 100) # Buffer set with limit 100 
-        data = buff.decode() # makes string interpretable
-    
-    while index < len(data):
-        current_char = data[index]
+    buff = read(0, 100) # Buffer set with limit 100
+    string = buff.decode() # makes string interpretable
+
+    while index < len(string):
+        current_char = string[index]
         if current_char == '\n': # checks for newline char
             return line
-        
+
         line += current_char
         index += 1;
-        
+
         if index == 100:
             buff =  read(0,100)
-            data = buff.decode()
+            string = buff.decode()
             index = 0
-    
+        
     return "" # EOF reached
